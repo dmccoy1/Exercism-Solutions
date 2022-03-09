61d5f5e83bbcd9db48dc30d8ae4e99cdeea04afe
Darius McCoy <dariusmccoy@Betamores-Air-2.daintytoaster.com>
2022-03-09 13:23:30 -0500

    added solution

---
+++ mixed-juices/mixed-juices.js
@@ -0,0 +1,87 @@
+// @ts-check
+//
+// The line above enables type checking for this file. Various IDEs interpret
+// the @ts-check directive. It will give you helpful autocompletion when
+// implementing this exercise.
+
+/**
+ * Determines how long it takes to prepare a certain juice.
+ *
+ * @param {string} name
+ * @returns {number} time in minutes
+ */
+export function timeToMixJuice(name) {
+  switch (name) {
+    case 'Pure Strawberry Joy':
+      return 0.5
+    case 'Energizer':
+      return 1.5
+    case 'Green Garden':
+      return 1.5
+    case 'Tropical Island':
+      return 3
+    case 'All or Nothing':
+      return 5
+    default:
+      return 2.5
+
+
+  }
+}
+
+/**
+ * Calculates the number of limes that need to be cut
+ * to reach a certain supply.
+ *
+ * @param {number} wedgesNeeded
+ * @param {string[]} limes
+ * @returns {number} number of limes cut
+ */
+export function limesToCut(wedgesNeeded, limes) {
+  let limesCut = [];
+
+  for (let i = 0; i < limes.length; i++) {
+    let limeWedges = 0;
+    switch (limes[i]) {
+      case 'small':
+        limeWedges = 6;
+        break;
+      case 'medium':
+        limeWedges = 8;
+        break;
+      case 'large':
+        limeWedges = 10;
+        break;
+    }
+    limesCut.push(limeWedges);
+  }
+
+  let limeTotal = 0;
+  let wedges = 0;
+
+  while (wedges < wedgesNeeded) {
+    wedges += limesCut[limeTotal];
+    if (limeTotal < limesCut.length) {
+      limeTotal++;
+    }
+  }
+
+  return limeTotal;
+}
+
+
+/**
+ * Determines which juices still need to be prepared after the end of the shift.
+ *
+ * @param {number} timeLeft
+ * @param {string[]} orders
+ * @returns {string[]} remaining orders after the time is up
+ */
+export function remainingOrders(timeLeft, orders) {
+  do {
+    timeLeft -= timeToMixJuice(orders[0]);
+    orders.shift();
+  } while (timeLeft > 0);
+  return orders
+}
+
